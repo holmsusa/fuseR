c5c3b6b7fd6666fe1e941ad9852e0d36027c4ae0
holmsusa <susanna.k.holmstrom@gmail.com>
2025-04-24 14:11:30 +0300

    Updated way of computing likelihood

--- src/island.c
+++ src/island.c
@@ -1,12 +1,25 @@
+/**
+ * @file island.c
+ * @brief Implementation of R-C interface functions.
+ * 
+ * This file contains functions that take in R variables and return R variables, 
+ * but are implemented in C.
+ * The functions in this file are for clustering, sorting trees, cutting trees, 
+ * and computing Pearson correlation row sums in an R environment.
+ */
 
-#include <R.h>
+
 #include <Rinternals.h>
-#include <R_ext/Rdynload.h>
 #include <assert.h>
-#include "libisland.h"  // Your additional custom header
+#include "libisland.h"
 
 
-
+/**
+ * @brief Checks if the given SEXP argument is a 2D structure.
+ * 
+ * @param arg An R object to check.
+ * @return int Returns nonzero if the argument is a vector or matrix, otherwise 0.
+ */
 static
 __attribute__((unused))
 int
@@ -14,159 +27,115 @@ is2D(SEXP arg)
 { return isVector(arg) || isMatrix(arg); }
 
 
-
+/**
+ * @brief Sorts a tree structure in R.
+ * 
+ * @param tree_R Input matrix representing the tree structure.
+ * @return SEXP A newly allocated matrix with the sorted tree.
+ */
 SEXP
-sort_tree_R(SEXP Z_R)
+sort_tree_R(SEXP tree_R)
 {
-	SEXP Z_new_R ; 
-	size_t m;
-	double *scratch;
+	SEXP tree_sorted_R ; 
+	size_t nrow;
+	double *scratch; // Scratch memory
 
-	Z_new_R = PROTECT(duplicate(Z_R));
-	m = nrows(Z_R);
+	tree_sorted_R = PROTECT(duplicate(tree_R));
+	nrow = nrows(tree_R);
 
-	scratch = (double*)R_alloc(sizeof(*scratch), sort_tree_size(m));
+	scratch = (double*)R_alloc(sizeof(*scratch), sort_tree_size(nrow));
 
-	sort_tree( REAL(Z_new_R), scratch, REAL(Z_R), m );
-
-	//return Z_new_R;
+	sort_tree( REAL(tree_sorted_R), scratch, REAL(tree_R), nrow );
 
 	UNPROTECT(1);
-	return Z_new_R;
+	return tree_sorted_R;
 }
 
+/**
+ * @brief Performs hierarchical clustering on input matrices.
+ * 
+ * @param K0_R Integer matrix representing the unmethylated counts.
+ * @param K1_R Integer matrix representing the methylated counts.
+ * @param CHR_R Integer vector of chromosome identifiers.
+ * @param POS_R Integer vector of position identifiers.
+ * @return SEXP A matrix with 5 columns containing clustering results.
+ * 			1. label for merged point 1
+ * 			2. label for merged point 2
+ * 			3. Change in total likelihood
+ * 			4. Total cost of merge
+ * 			5. Genomic distance between points
+ */
 SEXP 
-island_cluster_R(SEXP K0_R, SEXP K1_R, SEXP CHR_R, SEXP POS_R)
+fuse_cluster_R(SEXP K0_R, SEXP K1_R, SEXP CHR_R, SEXP POS_R)
 {
+	int nrow, ncol;
+	SEXP tree_R;
 
-	
-	int m, n;
-	SEXP Z_R;
-
-	/* check type for input */
+	/* Check type of input */
 	if (!(isInteger(K0_R)))
 		error("'%s' must be an integer array", "K0");
 	if (!(isInteger(K1_R)))
 		error("'%s' must be an integer array", "K1");
 
-
 	/* Get dimensions */
-	m = nrows(K0_R);
-	n = ncols(K0_R);
+	nrow = nrows(K0_R);
+	ncol = ncols(K0_R);
 
 
 	/* Check dimensions */
-	if (!(nrows(K0_R) == m && ncols(K0_R) == n))
-		error("'%s' must be %d-by-%d", "K0", m, n);
-	if (!(nrows(K1_R) == m && ncols(K1_R) == n))
-		error("'%s' must be %d-by-%d", "K1", m, n);
+	if (!(nrows(K0_R) == nrow && ncols(K0_R) == ncol))
+		error("'%s' must be %d-by-%d", "K0", nrow, ncol);
+	if (!(nrows(K1_R) == nrow && ncols(K1_R) == ncol))
+		error("'%s' must be %d-by-%d", "K1", nrow, ncol);
 
-	/* Allocate memory */
-	Z_R = PROTECT(allocMatrix(REALSXP, m-1, 4));
+	/* Allocate memory for output */
+	tree_R = PROTECT(allocMatrix(REALSXP, nrow-1, 5)); // It takes n-1 merges to merge n points
 
 	/* Run clustering */
-	island_cluster( REAL(Z_R), INTEGER(K0_R), INTEGER(K1_R), INTEGER(CHR_R), INTEGER(POS_R), m, n );
+	island_cluster( REAL(tree_R), INTEGER(K0_R), INTEGER(K1_R), INTEGER(CHR_R), INTEGER(POS_R), nrow, ncol );
 
 	UNPROTECT(1);
-	return Z_R;
+	return tree_R;
 }
 
+/**
+ * @brief Cuts a hierarchical tree into clusters.
+ * 
+ * @param tree_R Input matrix representing the tree structure.
+ * @param num_of_clust_R Number of clusters.
+ * @return SEXP A vector of cluster labels.
+ */
 SEXP
-cuttree_R(SEXP Z_R, SEXP K_R) 
+cuttree_R(SEXP tree_R, SEXP num_of_clust_R) 
 {
-	size_t *L, k, m;
-
-	SEXP L_R;
+	size_t *labels, num_of_clust, num_of_points;
+	SEXP labels_R;
 
 	/* Dimensions */
-	m = nrows(Z_R) + 1.;
-	k = (size_t) INTEGER(K_R)[0];
-
- 
-	/* Allocate size_t *L and SEXP or int *L. Compute size_t L with function, return int L + 1 since R starts from 1 not 0 */
+	num_of_points = nrows(tree_R) + 1.; // It takes n steps to merge n+1 points 
+	num_of_clust = (size_t) INTEGER(num_of_clust_R)[0];
 	
 	/* Allocate memory */
-	L_R = PROTECT(allocVector(INTSXP, m));
-	L = (size_t*)R_alloc(sizeof(*L), 2*m);
-
+	labels_R = PROTECT(allocVector(INTSXP, num_of_points));
+	labels = (size_t*)R_alloc(sizeof(*labels), 2*num_of_points);
 
 	/* Cut tree */
-	// L: cluster labels, Z: tree, k: number of clusters, m: number of sites
-	cutree(L, REAL(Z_R), k, m); // <-- FAILS HERE
+	cutree(labels, REAL(tree_R), num_of_clust, num_of_points); 
 
-	/* Copy into other vector */
-	for(size_t i = 0; i < m; ++i) { // for(size_t i = 0; i <=m; ++i) {
-		INTEGER(L_R)[i] = (int)L[i] + 1; // <-- Fails here
+	/* Copy into output vector */
+	for(size_t i = 0; i < num_of_points; ++i) { 
+		INTEGER(labels_R)[i] = (int)labels[i] + 1; // C labels start from 0, R labels from 1
 	}
 
 	UNPROTECT(1);
-	return L_R;
-	
-}
-
-SEXP
-cor_pearson_rowSums_R(SEXP x_R, SEXP Y_R) 
-{
-	int m, nx, ny;
-	SEXP sxy_R, sy1_R, sy2_R, res_R;
-
-	/* Dimensions */
-	m = nrows(x_R); // Number of samples
-	nx = ncols(x_R); // NUmber of segments in X
-	ny = ncols(Y_R); // Number of segments in Y
-
-	if (!(nrows(x_R) == m && ncols(x_R) == 1))
-		error("x must be vector");
-	
-	if (!(nrows(Y_R) == m && ncols(Y_R) == ny))
-		error("y must have same dimension as x");
-
-	if (!isReal(x_R))
-		error("x must be doubles");
-	if (!isReal(Y_R))
-		error("y must be doubles");
-
-	(void)nx;
-	
-	/* Allocate memory for output */
-	sxy_R = PROTECT(allocVector(REALSXP, ny));
-	sy1_R = PROTECT(allocVector(REALSXP, ny));
-	sy2_R = PROTECT(allocVector(REALSXP, ny));
-	
-
-
-	/* Correlation */
-	cor_pearson_rowSums(REAL(sxy_R), REAL(sy1_R), REAL(sy2_R), REAL(x_R), REAL(Y_R), m, ny) ;
-
-
-	/* Create output tuple */
-	res_R = PROTECT(allocList(3));
-	SETCAR(res_R, sy1_R);
-	SET_TAG(res_R, install("sy1"));
-	SETCAR(CDR(res_R), sy2_R);
-	SET_TAG(CDR(res_R), install("sy2"));
-	SETCAR(CDDR(res_R), sxy_R);
-	SET_TAG(CDDR(res_R), install("sxy"));
-
-	UNPROTECT(4);
-	return res_R;
+	return labels_R;
 }
 
 
-// Register routines
-static const R_CallMethodDef callMethods[] = {
-  {"island_cluster_R", (DL_FUNC) &island_cluster_R, 4},
-  {"sort_tree_R",      (DL_FUNC) &sort_tree_R,      1},
-  {"cuttree_R",        (DL_FUNC) &cuttree_R,        2},
-  {"cor_pearson_rowSums_R", (DL_FUNC) &cor_pearson_rowSums_R, 2},
-  {NULL, NULL, 0}
-};
 
 
-void R_init_island(DllInfo *dll) {
-  R_registerRoutines(dll, NULL, callMethods, NULL, NULL);
-  R_useDynamicSymbols(dll, FALSE);
-}
+
+
 
 
 
